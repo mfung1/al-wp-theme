f7c103920071c216ac1a012235d66845560c5b04
= <=>
2018-10-20 17:13:02 +0100

    Add basic hero compoenent

--- wp-content/themes/annette-lankester-2018/components/billboard.php
+++ wp-content/themes/annette-lankester-2018/components/billboard.php
@@ -1,7 +1,8 @@
 <article class="al-hero">
   <div class="al-hero-cpy">
     <h1 class="al-o1 col-purp">Welcome</h1>
-    <p class="al-t1">Get the help you need to overcome your troubles, and set your life back on course</p>
+    <p class="al-t1 mb-12">Get the help you need to overcome your troubles, and set your life back on course</p>
+    <a href="#" class="al-btn col-wht"><span class="al-t2">Find out more</span></a>
   </div>
   <div class="al-hero-img"></div>
 </article>
