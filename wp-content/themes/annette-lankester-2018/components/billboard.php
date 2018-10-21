1d023b23a825bb067afec43cba8a709cb5200abd
= <=>
2018-10-21 13:49:13 +0100

    Add teriary areas

--- wp-content/themes/annette-lankester-2018/components/billboard.php
+++ wp-content/themes/annette-lankester-2018/components/billboard.php
@@ -1,8 +1,51 @@
-<article class="al-hero al-hero-nrml">
-  <div class="al-hero-cpy">
-    <h1 class="al-o1 mb-9 col-purp">Welcome</h1>
-    <p class="al-t1 mb-12">Get the help you need to overcome your troubles, and set your life back on course</p>
-    <a href="#" class="al-btn al-btn-prp col-wht"><span class="al-t2">Find out more</span></a>
-  </div>
-  <div class="al-hero-img"></div>
-</article>
+<?php
+$args = array (
+  'post_per_page' => '3',
+  'category_name' => 'homepage-banner',
+  'order'         => 'ASC'
+);
+
+$the_query = new WP_Query($args);
+$i = 0;
+?>
+<?php if($the_query->have_posts()): while($the_query->have_posts()): $the_query->the_post();?>
+  <article class="al-hero <?php echo($i == 1 ? 'al-hero-rvrs' : '') ?>">
+  <?php
+  $arr = array (
+    "0 0 472 312",
+    "0 0 454 312",
+    "0 0 451 313"
+  );
+  if($i == 1):?>
+  <?php
+    $postid = get_the_ID();
+    $postmeta = get_post_meta($postid, 'Image path');
+    $imgpath = get_stylesheet_directory_uri() . $postmeta[0];
+    ?>
+    <div class="al-hero-svg">
+      <svg viewbox="<?php echo $arr[$i];?>" preserve-aspect-ratio="xMidYMid meet">
+        <use xlink:href="<?php echo $imgpath ?>"/>
+      </svg>
+    </div>
+    <div class="al-hero-cpy">
+      <h2 class="mb-9"><?php the_title() ?></h2>
+      <p class="al-t2"><?php echo get_the_content() ?></p>
+    </div>
+<?php else: ?>
+   <div class="al-hero-cpy">
+      <h2 class="mb-9"><?php the_title() ?></h2>
+      <p class="al-t2"><?php echo get_the_content() ?></p>
+    </div>
+    <?php
+    $postid = get_the_ID();
+    $postmeta = get_post_meta($postid, 'Image path');
+    $imgpath = get_stylesheet_directory_uri() . $postmeta[0];
+    ?>
+    <div class="al-hero-svg">
+      <svg viewbox="<?php echo $arr[$i];?>" preserve-aspect-ratio="xMidYMid meet">
+        <use xlink:href="<?php echo $imgpath ?>"/>
+      </svg>
+    </div>
+<?php endif;?>
+  </article>
+  <?php $i++; endwhile; endif; ?>
---
+++ wp-content/themes/annette-lankester-2018/components/hero.php
@@ -0,0 +1,8 @@
+<article class="al-hero al-hero-nrml">
+  <div class="al-hero-cpy">
+    <h1 class="al-o1 mb-9 col-purp">Welcome</h1>
+    <p class="al-t1 mb-12">Get the help you need to overcome your troubles, and set your life back on course.</p>
+    <a href="#" class="al-btn al-btn-prp col-wht"><span class="al-t2">Find out more</span></a>
+  </div>
+  <div class="al-hero-img"></div>
+</article>
