51d85c9332a2849d5585a606c68234466fede57c
= <=>
2018-11-10 16:02:17 +0000

    Add let's talk banner

--- wp-content/themes/annette-lankester-2018/components/lets-talk.php
+++ wp-content/themes/annette-lankester-2018/components/lets-talk.php
@@ -1,15 +1,15 @@
 <?php
-$imgpath = get_stylesheet_directory_uri() . '/img/gift.svg#gift';
+$imgpath = get_stylesheet_directory_uri() . '/img/chat-bubbles.svg#chat-bubbles';
 ?>
-<article class="al-hero al-hero-md al-hero-fwmw-960">
+<article class="mw-960 al-hero al-hero-md al-hero-fw">
+  <div class="al-hero-cpy">
+    <h2 class="col-purp mb-9">Get in touch</h2>
+    <p class="al-t2 mb-12 col-purp">If you think I can help you, just drop me a note. I&rsquo;ll get back to you as soon as I can.</p>
+    <a href="#" class="al-btn al-btn-prp col-wht"><span class="al-t3">Let&rsquo;s chat</span></a>
+  </div>
   <div class="al-hero-svg">
-    <svg preserveAspectRatio="xMidYMid meet" viewbox="0 0 369 345">
+    <svg preserveAspectRatio="xMidYMid meet" viewbox="0 0 343 250">
       <use xlink:href="<?php echo $imgpath?>"/>
     </svg>
-  </div>
-  <div class="al-hero-cpy">
-    <h2 class="col-wht mb-9">Get in touch</h2>
-    <p class="al-t2 mb-12 col-wht">If you tink I can help you, just drop me a note. I'll get back to you as soon as I can.</p>
-    <a href="#" class="al-btn al-btn-prp-lt col-wht"><span class="al-t3">Let&rsquo;s chat</span></a>
   </div>
 </article>
--- wp-content/themes/annette-lankester-2018/page-fees.php
+++ wp-content/themes/annette-lankester-2018/page-fees.php
@@ -21,5 +21,22 @@
   <section class="mw-1200 mb-36">
     <?php get_template_part('components/payment', 'fees') ?>
   </section>
+  <section class="bg-purp-lt mb-36">
+    <?php get_template_part('components/lets-talk', 'fees') ?>
+  </section>
+  <section class="mw-1200 mt-52 mb-36">
+    <h2 class="al-h2 mb-19">Services</h2>
+    <p class="al-t2 mb-19">I am a fully train Hypnotherapist and Pyschotherapeautic counsellor. As such, I offer a variety of expertise across both spectrums.</p>
+    <h3 class="al-h3 mb-19">Counselling</h3>
+    <p class="al-t2 mb-19">I believe that our mental health deserves as much attention as our physical health. Talking therapy is one way of giving it the care it needs.</p>
+    <p class="al-t2 mb-19">Life brings many changes; some of which can leave us feeling unable to cope; causing mixed emotions to run high.</p>
+    <svg preserveAspectRatio="xMidYMid meet" viewbox="0 0 343 250">
+      <?php
+      $img = get_stylsheet_direct_uri() . '/img/friends.svg#friends'
+      ?>
+      <use xlink:href="<?php echo $img; ?>"/>
+    </svg>
+
+  </section>
 
 <?php get_footer();?>
